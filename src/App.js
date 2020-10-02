5f721ae877001eb195922cd5737642252078815c
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2020-10-02 09:12:25 -0700

    dropdown created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Accordion from './components/Accordion';
-import Search from './components/Search'
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
   {
@@ -17,12 +18,28 @@ const items = [
     title: 'How do you use React?',
     content: 'with your brain.'
   }
-]
+];
+
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'The Color Blue',
+    value: 'blue'
+  }
+];
 
 export default () => {
   return (
     <div>
     {/* <Accordion items={items} /> */}
-    <Search />
+    {/* <Search /> */}
+    <Dropdown options={options} />
   </div>)
 };
